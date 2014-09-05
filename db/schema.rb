dd838ce87bce87e505d9479e3bdffbf1abd0e9e9
Jay Hayes <ur@iamvery.com>
2014-09-04 23:55:27 -0500

    Add articles table to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140904212536) do
+
+  create_table "articles", force: true do |t|
+    t.string   "title",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
